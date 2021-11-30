4b02d42c198684680200fbf46af515bc6b1d9dd7
Felipe Merchan <felipe-sc@hotmail.es>
2021-11-29 22:45:55 -0500

    style: add animations

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -9,9 +9,9 @@ export const GifGrid = ({ category }) => {
 
   return (
     <section aria-label="Gif list">
-      <h3>{category}</h3>
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
 
-      {loading && 'Cargando...'}
+      {loading && <p className="animate__animated animate__flash">Cargando...</p>}
 
       <div className="card-grid">
         {
--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifItem = ({ title, url }) => {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__fadeIn">
       <img
         className="card__img"
         src={url}
